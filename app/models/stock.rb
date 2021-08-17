367d0ec85c11f2d3c186384e3e9a2846a66e3e73
harsh291199 <harshsuthar2911@gmail.com>
2021-08-17 19:08:53 +0530

    Add Stock model and iex api key

---
+++ app/models/stock.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Stock model
+class Stock < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_816_072_304) do
+ActiveRecord::Schema.define(version: 20_210_817_120_546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'stocks', force: :cascade do |t|
+    t.string 'ticker'
+    t.string 'name'
+    t.decimal 'last_price'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+  end
 
   create_table 'users', force: :cascade do |t|
     t.string 'email', default: '', null: false
