490ed5065c13bc02cb15be58d3134b9c2a786add
harsh291199 <harshsuthar2911@gmail.com>
2021-08-19 19:13:23 +0530

    Build portfolio view and Add stock lookup form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Users controller
+class UsersController < ApplicationController
+  def my_portfolio; end
+end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,8 +3,12 @@
 # Stock model
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:cloud_api_key],
-                                  endpoint: 'https://cloud.iexapis.com/v1')
-    client.price(ticker_symbol) 
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+    begin 
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exeception
+      return nil
+    end
   end
 end
