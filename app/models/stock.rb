0e48cd594dab2f21a4d2f69b1b0880c40cb012f5
harsh291199 <harshsuthar2911@gmail.com>
2021-08-19 13:12:02 +0530

    Add changes to Stock model and store secure API key

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,4 +2,9 @@
 
 # Stock model
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:cloud_api_key],
+                                  endpoint: 'https://cloud.iexapis.com/v1')
+    client.price(ticker_symbol) 
+  end
 end
